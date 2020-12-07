b99484dcd5badfb942e9e6d655714fd465070b2a
yoonsangkwak <yunsang08@naver.com>
2020-12-07 17:04:15 +0900

    alog html css js update

---
+++ static/main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+// ​const section = document.querySelector('.section');
+const section = document.getElementById('section');
+const clientRect = section.getBoundingClientRect(); // DomRect 구하기 (각종 좌표값이 들어있는 객체)
+const relativeTop = clientRect.top; // Viewport의 시작지점을 기준으로한 상대좌표 Y 값.
+const absoluteTop = window.pageYOffset + relativeTop; // 절대좌표
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle('on');
+});
+
+window.addEventListener("scroll", () => {
+    if (pageYOffset >= absoluteTop) {
+        menu.classList.remove('on');
+    }
+});
