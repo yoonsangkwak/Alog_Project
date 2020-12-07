af96c2eed8743ad7f7c59f3d7df73ef0e6ef40be
yoonsangkwak <yunsang08@naver.com>
2020-12-07 21:44:46 +0900

    signup

--- alog/views/answer_views.py
+++ alog/views/answer_views.py
@@ -10,5 +10,15 @@ def answer_create(request, blogpost_id):
     댓글등록
     """
     blogpost = get_object_or_404(Blogpost, pk=blogpost_id)
-    blogpost.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('alog:detail', blogpost_id=blogpost.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.blogpost = blogpost
+            answer.save()
+            return redirect('alog:detail', blogpost_id=blogpost.id)
+    else:
+        form = AnswerForm()
+    context = {'blogpost': blogpost, 'form': form}
+    return render(request, 'alog/post_detail.html', context)
