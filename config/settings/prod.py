037594936777fed682cfa8914e69d3cdcd516d91
yoonsangkwak <yunsang08@naver.com>
2020-12-08 18:35:07 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.180.225.243']
+ALLOWED_HOSTS = ['54.180.225.243', 'yoonsang.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
